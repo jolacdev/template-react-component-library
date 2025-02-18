a17cee064b4457980377516e9ee03a7ee73a414d
Jose L <58451072+jolacdev@users.noreply.github.com>
2025-02-18 21:45:04 +0100

    fix: test

--- release.config.mjs
+++ release.config.mjs
@@ -12,7 +12,12 @@ export default {
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'CHANGELOG.md'],
+        assets: [
+          'package.json',
+          'README.md',
+          'CHANGELOG.md',
+          'dist/**/*.{js|ts|css}',
+        ],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
