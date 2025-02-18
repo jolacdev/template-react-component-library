ceaaecf95b45e70308a73830819dd29a29de1832
Jose L <58451072+jolacdev@users.noreply.github.com>
2025-02-18 19:29:12 +0100

    ci: :construction_worker: Add CD workflow with automatic semantic release

---
+++ release.config.mjs
@@ -0,0 +1,21 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ['github-actions'],
+  plugins: [
+    '@semantic-release/commit-analyzer', // Included in semantic-release.
+    '@semantic-release/release-notes-generator', // Included in semantic-release.
+    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+    '@semantic-release/npm', // Included in semantic-release.
+    '@semantic-release/github', // Included in semantic-release.
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
